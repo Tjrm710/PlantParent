0c3c9c6c2488f8fdf15cf3b3ac601eb710877318
Tjrm710 <65799712+Tjrm710@users.noreply.github.com>
2021-05-29 21:54:30 -0500

    Update Gitplantparent.py

--- Gitplantparent.py
+++ Gitplantparent.py
@@ -1,5 +1,8 @@
+#import requests to handle pulling from the openweathermap API, Import Json in order to filter the get request into the specific information we need
 import requests, json
+# Yagmail is an easy way for you to send emails from a python script, compared to using a gmail API. Keyring enables you to keep your username and password for your account secure, encrypted, and out of your script
 import yagmail, keyring
+# Schedule is what we use to enable the script to run every day at 12:38. 
 import schedule
 import time
 #
@@ -11,7 +14,7 @@ def Notification():
     Textmessage = str(Temperature)
     Temperature_alert = 80
     if Temperature < Temperature_alert:
-        yagmail.SMTP('Yourgmail@gmail.com').send('1234567890@examplegateway.com', '[your title]',"Hi Tjrm710, The low for today will be "+ Textmessage +"°F, Stay Warm!")
+        yagmail.SMTP('Yourgmail@gmail.com').send('1234567890@examplegateway.com', '[your title]',"Hi Tjrm710, The low for today will be "+ Textmessage +"°F, Stay cool!")
 #
 #
 # Schedules the program to run at a set time every single day
